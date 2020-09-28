249e021f3d4a71963717f75e50b78f9859ee9c59
Mia-Salazar <maria.sgr@gmail.com>
2020-09-28 22:09:02 +0200

    Create superpower page

--- src/app/pages/superpower/superpower.component.ts
+++ src/app/pages/superpower/superpower.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-superpower',
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./superpower.component.scss']
 })
 export class SuperpowerComponent implements OnInit {
+  mobile = true;
+  scroll = false;
+  weaknessOpen = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  valueBar(num){
+    if (this.mobile || this.scroll) {
+      return num
+    }
+  }
+
+  @HostListener("window:scroll", [])onWindowScroll() {
+    if (window.innerWidth > 767) {
+      this.mobile = false;
+      if(window.scrollY > 900) {
+        this.scroll = true;
+      } else  {
+        this.scroll = false;
+      }
+    }   
   }
 
 }
