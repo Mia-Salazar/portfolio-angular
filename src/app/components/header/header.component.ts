8dc88a4b10de45232f334cb8afe8dcd6b1943aba
Mia <maria.sgr@gmail.com>
2021-01-17 23:55:01 +0100

    Add button for changing lang

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  language: string = 'en';
 
-  constructor() { }
+  constructor(private translate: TranslateService) { }
 
   ngOnInit(): void {
   }
 
+  useLanguage(language: string) {
+    console.log(language)
+    this.language = language;
+    this.translate.use(language);
+  }
 }
