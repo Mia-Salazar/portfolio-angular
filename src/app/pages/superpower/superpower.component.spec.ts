c0f504e0d0ed0dbf6c4696abf00019e80a45d91f
Mia <maria.sgr@gmail.com>
2021-01-17 22:11:57 +0100

    Update Angular to 11

--- src/app/pages/superpower/superpower.component.spec.ts
+++ src/app/pages/superpower/superpower.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SuperpowerComponent } from './superpower.component';
 
@@ -6,7 +6,7 @@ describe('SuperpowerComponent', () => {
   let component: SuperpowerComponent;
   let fixture: ComponentFixture<SuperpowerComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SuperpowerComponent ]
     })
