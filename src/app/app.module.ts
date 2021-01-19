dbfeff65cb24369320787e4ddb95abc3c1d2acd2
Mia <maria.sgr@gmail.com>
2021-01-19 13:41:17 +0100

    Reorder components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,41 +9,27 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { SocialLinkComponent } from './components/social-link/social-link.component';
-import { ContactFormComponent } from './components/contact-form/contact-form.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { SuperpowerComponent } from './pages/superpower/superpower.component';
 import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 import { AwardsComponent } from './pages/awards/awards.component';
 import { HomeComponent } from './pages/home/home.component';
-import { BoxComponent } from './components/box/box.component';
-import { HeadingComponent } from './components/heading/heading.component';
-import { BarComponent } from './components/bar/bar.component';
 import { ErrorComponent } from './pages/error/error.component';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    HeaderComponent,
-    FooterComponent,
-    SocialLinkComponent,
-    ContactFormComponent,
     ContactComponent,
     SuperpowerComponent,
     PortfolioComponent,
     AwardsComponent,
     HomeComponent,
-    BoxComponent,
-    HeadingComponent,
-    BarComponent,
     ErrorComponent,
   ],
   imports: [
     BrowserModule,
+    ComponentsModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
---
+++ src/app/components/components.module.ts
@@ -0,0 +1,62 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { RecaptchaFormsModule, RecaptchaModule } from 'ng-recaptcha';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SocialLinkComponent } from './social-link/social-link.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { BoxComponent } from './box/box.component';
+import { HeadingComponent } from './heading/heading.component';
+import { BarComponent } from './bar/bar.component';
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    HeaderComponent,
+    FooterComponent,
+    SocialLinkComponent,
+    ContactFormComponent,
+    BoxComponent,
+    HeadingComponent,
+    BarComponent,
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    RecaptchaModule,
+    FormsModule,
+    BrowserModule,
+    RecaptchaFormsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ],
+  exports: [
+    NavbarComponent,
+    HeaderComponent,
+    FooterComponent,
+    SocialLinkComponent,
+    ContactFormComponent,
+    BoxComponent,
+    HeadingComponent,
+    BarComponent
+  ]
+})
+export class ComponentsModule { }
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
